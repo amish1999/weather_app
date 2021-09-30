0ad63fa4600449e4e09426e990dfe7d1ec8af18d
Amish Bharti <47895005+amish1999@users.noreply.github.com>
2021-09-30 23:02:07 +0530

    Update server.js

--- weather_app/server.js
+++ weather_app/server.js
@@ -14,24 +14,14 @@ app.get('/', function (req, res) {
   res.render('index', {weather: null, error: null});
 })
 
-app.post('/', function (req, res) {
-  let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+<!--------------->
 
-  request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
-    } else {
-      let weather = JSON.parse(body)
-      if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Error, please try again'});
-      } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
-      }
-    }
-  });
-})
+
+
+
+
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
